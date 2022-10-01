8c274d41c17c9779a00466c3cca3de20e7d11687
hwihun.cho <hwihun.cho@navercorp.com>
2022-10-01 20:23:12 +0900

    msw.js 경로 찾는 로직 수정 및 스토리에 rest api 모킹해봄

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,15 +1,22 @@
 import { initialize, mswDecorator } from 'msw-storybook-addon';
 
 // Initialize MSW
-console.log('msw init 하기 전');
-console.log(`process.env.NODE_ENV: ${process.env.NODE_ENV}`)
-const { origin, pathname } = location
-const mswWorkerPath = process.env.NODE_ENV === 'development' ? '' : `${origin}${pathname}`
 initialize({
   serviceWorker: {
-    url: `${mswWorkerPath}mockServiceWorker.js`,
-  }
-});
+    // serviceWorker.url의 기본 값은 '/mockServiceWorker.js'이다.
+    // 스토리북을 도메인의 루트가 아니라 하위 디렉토리에서 실행하는 경우는 이 파일을 찾지 못하는 이슈가 있다.
+    // (상대 경로로 지정하면 찾기는 하는데 제대로 초기화가 되지 않는다.)
+    // 그래서 스토리북이 실행되고 있는 경로를 찾아서 절대경로로 지정해야 한다.
+    // 스토리는 iframe에서 실행되는데 그래서 상위 window의 location에서 pathname을 참조해야 한다.
+    // https://www.A.com/sub/ 에서 실행된다고 가정할 때. 이 preview.js가 실행되는 html은 https://www.A.com/sub/iframe.html 이므로
+    // 상위 window (window.parent)의 location을 참조해야 스토리북 빌드시의 public 폴더의 파일들에 접근할 수 있다.
+    url: (({ origin, pathname }) => {
+      const mswWorkerPath = `${origin}${pathname}`
+      console.log('[msw] mockServiceWorker.js 경로: ', `${mswWorkerPath}mockServiceWorker.js`)
+      return `${mswWorkerPath}mockServiceWorker.js`
+    })(window.parent.location),
+  },
+})
 
 // Provide the MSW addon decorator globally
 export const decorators = [mswDecorator];
--- src/stories/Button.stories.jsx
+++ src/stories/Button.stories.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { rest } from 'msw'
 import { Button } from './Button';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
@@ -13,7 +13,12 @@ export default {
 };
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template = (args) => <Button {...args} />;
+const Template = (args) => {
+  fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(json => console.log(json))
+  return <Button {...args} />
+};
 
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
@@ -21,6 +26,22 @@ Primary.args = {
   primary: true,
   label: 'Button',
 };
+Primary.parameters = {
+  msw: {
+    handlers: [
+      rest.get('https://jsonplaceholder.typicode.com/todos/1', (req, res, ctx) => {
+        return res(ctx.body(`
+          {
+            "userId": 1,
+            "id": 1,
+            "title": "this is mocked response",
+            "completed": false
+          }
+        `))
+      })
+    ]
+  },
+}
 
 export const Secondary = Template.bind({});
 Secondary.args = {
