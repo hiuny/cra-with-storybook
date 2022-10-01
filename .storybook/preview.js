8275055d216c629a8d4487299186285f65f69311
hwihun.cho <hwihun.cho@navercorp.com>
2022-10-01 22:22:08 +0900

    feat(storybook): msw.js 파일 경로 지정 로직 변경

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -7,14 +7,18 @@ initialize({
     // 스토리북을 도메인의 루트가 아니라 하위 디렉토리에서 실행하는 경우는 이 파일을 찾지 못하는 이슈가 있다.
     // (상대 경로로 지정하면 찾기는 하는데 제대로 초기화가 되지 않는다.)
     // 그래서 스토리북이 실행되고 있는 경로를 찾아서 절대경로로 지정해야 한다.
-    // 스토리는 iframe에서 실행되는데 그래서 상위 window의 location에서 pathname을 참조해야 한다.
-    // https://www.A.com/sub/ 에서 실행된다고 가정할 때. 이 preview.js가 실행되는 html은 https://www.A.com/sub/iframe.html 이므로
-    // 상위 window (window.parent)의 location을 참조해야 스토리북 빌드시의 public 폴더의 파일들에 접근할 수 있다.
+    // https://www.A.com/sub/ 에서 실행된다고 가정할 때, mockServiceWorker.js의 올바른 경로는
+    // https://www.A.com/sub/mockServiceWorker.js 이다.
+    // 이 파일(preview.js)에서 location.pathname을 출력해보면 'https://www.A.com/sub/iframe.html'이라고 나오므로
+    // 'iframe.html' 문자열을 ''으로 치환하는 로직을 사용한다.
+    // 상위 윈도우의 location(windpw.parent.location)에 접근하여 pathname을 출력해보면 'https://www.A.com/sub/'가
+    // 나오므로 그대로 사용하면 문제가 없긴 한데, github actions에서 test-storybook 스크립트를 수행할 때는
+    // 뭐가 문젠지 모르겠으나 'https://www.A.com/sub/iframe.html'이 나와서 테스트가 실패하므로 그건 버린다..
     url: (({ origin, pathname }) => {
-      const mswWorkerPath = `${origin}${pathname}`
+      let mswWorkerPath = `${origin}${pathname}`.replace('iframe.html', '')
       console.log('[msw] mockServiceWorker.js 경로: ', `${mswWorkerPath}mockServiceWorker.js`)
       return `${mswWorkerPath}mockServiceWorker.js`
-    })(window.parent.location),
+    })(location),
   },
 })
 
